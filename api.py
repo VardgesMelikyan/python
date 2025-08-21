1841ea8cae996ef784763209aad4bc7419fd54f7
VardgesMelikyan <vark94@gmail.com>
2025-08-21 23:59:37 +0400

    create background api

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, BackgroundTasks, HTTPException
+from main import main
+import os
 app = FastAPI()
 
 # эндпоинт "/" — корень API
@@ -19,3 +20,11 @@ def get_products():
         {"id": 1, "name": "Кроссовки", "price": 4999},
         {"id": 2, "name": "Рюкзак", "price": 2999},
     ]
+
+
+@app.post("/run-scrape")
+def run_scrape(background: BackgroundTasks, token: str):
+    if token != os.getenv("CRON_TOKEN"):
+        raise HTTPException(status_code=403, detail="forbidden")
+    background.add_task(main)  # запускаем в фоне и сразу отвечаем
+    return {"status": "queued"}
